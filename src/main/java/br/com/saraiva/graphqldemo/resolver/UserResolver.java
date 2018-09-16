57c109a2dd904227ef52bd0e6a72e474dc7faa7b
Vinícius D Saraiva <viniciusdsaraiva@gmail.com>
2018-09-16 10:21:35 -0300

    filter last orders

--- src/main/java/br/com/saraiva/graphqldemo/resolver/UserResolver.java
+++ src/main/java/br/com/saraiva/graphqldemo/resolver/UserResolver.java
@@ -1,5 +1,6 @@
 package br.com.saraiva.graphqldemo.resolver;
 
+import static org.springframework.data.domain.Sort.Direction.DESC;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
@@ -7,6 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +16,6 @@ import com.coxautodev.graphql.tools.GraphQLResolver;
 
 import br.com.saraiva.graphqldemo.model.Order;
 import br.com.saraiva.graphqldemo.model.User;
-import br.com.saraiva.graphqldemo.repository.OrderRepository;
 
 /**
  * Created by sara on set, 2018
@@ -22,19 +23,18 @@ import br.com.saraiva.graphqldemo.repository.OrderRepository;
 @Component
 public class UserResolver implements GraphQLResolver<User> {
 
-	private final OrderRepository orderRepository;
 	private final MongoTemplate mongoTemplate;
 
 	@Autowired
-	public UserResolver(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
-		this.orderRepository = orderRepository;
+	public UserResolver(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
 	}
 
-	public List<Order> orders(User user) {
+	public List<Order> orders(User user, int last) {
 
 		return mongoTemplate.find(
-				query(where("_id").in(user.getOrders().orElse(Collections.emptyList()))),
+				query(where("_id").in(user.getOrders().orElse(Collections.emptyList())))
+						.limit(last).with(new Sort(DESC, "_id")),
 				Order.class);
 	}
 
